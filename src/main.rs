a8eab13932efa53e584a0bca5fc19fe0022f6959
Nathan Hare <me@laspruca.nz>
2021-05-11 16:14:41 +1200

    Grid

--- src/main.rs
+++ src/main.rs
@@ -9,40 +9,68 @@ use piston::event_loop::{EventSettings, Events};
 use piston::input::{RenderArgs, RenderEvent, UpdateArgs, UpdateEvent};
 use piston::window::WindowSettings;
 
+
+#[derive(Clone, Debug)]
+enum Tile {
+    Red,
+    Green,
+    Blue,
+    Empty,
+}
+
+impl Tile {
+    fn get_color(&self) -> [f32; 4] {
+        match self {
+            Tile::Red => {
+                [1.0, 0.0, 0.0, 1.0]
+            }
+            Tile::Green => {
+                [0.0, 1.0, 0.0, 1.0]
+            }
+            Tile::Blue => {
+                [0.0, 0.0, 1.0, 1.0]
+            },
+            Tile::Empty => {
+                [0.0, 0.0, 0.0, 0.0]
+            }
+        }
+    }
+}
+
 pub struct App {
     gl: GlGraphics, // OpenGL drawing backend.
-    rotation: f64,  // Rotation for the square.
+    grid: Vec<Vec<Tile>>
 }
 
 impl App {
     fn render(&mut self, args: &RenderArgs) {
         use graphics::*;
 
-        const GREEN: [f32; 4] = [0.0, 1.0, 0.0, 1.0];
-        const RED: [f32; 4] = [1.0, 0.0, 0.0, 1.0];
-
-        let square = rectangle::square(0.0, 0.0, 50.0);
-        let rotation = self.rotation;
-        let (x, y) = (args.window_size[0] / 2.0, args.window_size[1] / 2.0);
+        let square = rectangle::square(0.0, 0.0, 25.0);
+        let (x, y) = (10.0, 10.0);
+        let grid = self.grid.clone();
 
         self.gl.draw(args.viewport(), |c, gl| {
             // Clear the screen.
-            clear(GREEN, gl);
+            clear([1.0, 1.0, 1.0, 1.0], gl);
 
-            let transform = c
-                .transform
-                .trans(x, y)
-                .rot_rad(rotation)
-                .trans(-25.0, -25.0);
+            for (y_offset, row) in grid.iter().enumerate() {
+                for (x_offset, tile) in row.iter().enumerate() {
+                    let transform = c
+                        .transform
+                        .trans(x, y)
+                        .trans(x_offset as f64 * 26.0, y_offset as f64* 26.0);
 
-            // Draw a box rotating around the middle of the screen.
-            rectangle(RED, square, transform, gl);
+                    // Draw a box rotating around the middle of the screen.
+                    rectangle(tile.get_color(), square, transform, gl);
+                }
+            }
         });
     }
 
     fn update(&mut self, args: &UpdateArgs) {
         // Rotate 2 radians per second.
-        self.rotation += 2.0 * args.dt;
+
     }
 }
 
@@ -51,7 +79,7 @@ fn main() {
     let opengl = OpenGL::V3_2;
 
     // Create an Glutin window.
-    let mut window: Window = WindowSettings::new("spinning-square", [200, 200])
+    let mut window: Window = WindowSettings::new("spinning-square", [800, 500])
         .graphics_api(opengl)
         .exit_on_esc(true)
         .build()
@@ -60,7 +88,13 @@ fn main() {
     // Create a new game and run it.
     let mut app = App {
         gl: GlGraphics::new(opengl),
-        rotation: 0.0,
+        grid: (2..22).map(|a| (1..13).map(|b| match (a * b) % 4 {
+            0 => Tile::Empty,
+            1 => Tile::Blue,
+            2 => Tile::Green,
+            3 => Tile::Red,
+            _ => Tile::Empty
+        }).collect()).collect()
     };
 
     let mut events = Events::new(EventSettings::new());
@@ -74,3 +108,4 @@ fn main() {
         }
     }
 }
+
